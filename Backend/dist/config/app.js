8e70ce528069f3e85f1c3c6f3e4b74580178464f
Maud <johansson.maud98@gmail.com>
2020-10-14 14:50:37 +0200

    backend oppsett ferdig

---
+++ Backend/dist/config/app.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const bodyParser = require("body-parser");
+const test_routes_1 = require("../routes/test_routes");
+class App {
+    constructor() {
+        this.test_routes = new test_routes_1.TestRoutes();
+        this.app = express();
+        this.config();
+        this.test_routes.route(this.app);
+    }
+    config() {
+        // support application/json type post data
+        this.app.use(bodyParser.json());
+        //support application/x-www-form-urlencoded post data
+        this.app.use(bodyParser.urlencoded({ extended: false }));
+    }
+}
+exports.default = new App().app;
