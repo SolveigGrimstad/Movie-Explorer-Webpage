d3241088d9ed74687dc48b45b8a5173ec90b51b9
Maud <johansson.maud98@gmail.com>
2020-10-14 09:39:12 +0200

    loginfail

---
+++ Frontend/src/header.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+function Header() {
+
+    return(
+        <div className="header" >
+            <h1 className="head">MovieSearch</h1>
+        
+        </div>
+        
+
+    )
+}
+
+
+export default Header;
