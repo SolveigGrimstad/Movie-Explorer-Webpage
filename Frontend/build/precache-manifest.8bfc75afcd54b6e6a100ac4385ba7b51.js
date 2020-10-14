2635f5906cd760a136f593668b014d1d0e0988e4
Maud <johansson.maud98@gmail.com>
2020-10-14 10:00:26 +0200

    commitfrombackend

---
+++ Frontend/build/precache-manifest.8bfc75afcd54b6e6a100ac4385ba7b51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "285121cec5717c39b857858a911e569b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ade263acabd37b41f7fa",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1b3534f9583970e2dce4",
+    "url": "/static/js/2.4442fcf5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4442fcf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ade263acabd37b41f7fa",
+    "url": "/static/js/main.9122b633.chunk.js"
+  },
+  {
+    "revision": "25f6b0a4266d9862c2c7",
+    "url": "/static/js/runtime-main.353b9f6a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
