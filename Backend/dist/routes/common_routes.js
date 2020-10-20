784119d1ffd4adc759afaffa713e7419a99ce002
Maud <johansson.maud98@gmail.com>
2020-10-20 12:24:45 +0200

    backend

--- Backend/dist/routes/common_routes.js
+++ Backend/dist/routes/common_routes.js
@@ -3,10 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.CommonRoutes = void 0;
 class CommonRoutes {
     route(app) {
-        // Mismatch URL
-        app.all('*', function (req, res) {
-            res.status(404).send({ error: true, message: 'Check your URL please' });
-        });
     }
 }
 exports.CommonRoutes = CommonRoutes;
