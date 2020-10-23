223f2007a3cea57d7d7b756a22209b42bc3a3784
Solveig Grimstad <solveiggrimstad@dhcp-10-220-222-115.wlan.ntnu.no>
2020-10-23 15:39:08 +0200

    movieinfo

--- Frontend/src/components/movieinfo.tsx
+++ Frontend/src/components/movieinfo.tsx
@@ -1,13 +1,48 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import * as React from "react";
+import {useDispatch, useSelector} from "react-redux";
+import { Person, addPerson, removePerson, AppState} from '../store/store'
 
-function Movieinfo(props: any) {
-  let id = useParams();
-  return (
-    <div>
-      <h1>Info om film</h1>
-    </div>
-  );
-}
+const Movieinfo = () => {
+    const [newPerson, setNewPerson] = React.useState("");
+    const updateNewPerson = () => (e: React.ChangeEvent<HTMLInputElement>) =>
+        setNewPerson(e.currentTarget.value);
+
+    const people: Person[] = useSelector((state: AppState) => state.people);
+    const movieId: string = useSelector((state: AppState) => state.movieId);
+    const dispatch = useDispatch();
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        dispatch(addPerson(newPerson));
+        setNewPerson("");
+    };
+
+    const dispatchNewPerson = (id: number) => () => {
+        dispatch(removePerson(id));
+    };
+
+    return (
+        <div>
+          
+            <ul>
+                {people.map(person => (
+                    <li key={person.id}>
+                        <span>{person.name}</span>
+                        <button onClick={dispatchNewPerson(person.id)}>Remove</button>
+                    </li>
+                ))}
+            </ul>
+            <form onSubmit={handleSubmit}>
+                <input
+                    placeholder="Enter name"
+                    value={newPerson}
+                    onChange={updateNewPerson()}
+                />
+                <button>Add</button>
+            </form>
+        </div>
+    );
+};
 
 export default Movieinfo;
+
