4c245bc0566972ec9ae75c3a687b07692efadc9d
Maud <johansson.maud98@gmail.com>
2020-10-29 12:42:26 +0100

    testing

---
+++ Frontend/src/test/content.test.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import renderer from 'react-test-renderer'; 
+import Content from '../components/content'; 
+
+/*
+it("renders correctly when there are no items", () => {
+    const tree = renderer.create(<Content 
+        Title = {'Skyfall'} 
+        Year = {"2012"}
+        Released =  "09 Nov 2012"
+        Runtime="143 min"
+        Genre= "Action, Adventure, Thriller"
+        Director = "Sam Mendes"
+        Actors ="Daniel Craig, Judi Dench, Javier Bardem, Ralph Fiennes"
+        Plot="When Bond's latest assignment goes gravely wrong and agents around the..."
+        Language = "English, Turkish, Shanghainese, Portuguese, Japanese"
+        Country = "UK, USA, Turkey"
+        Poster =  "https://m.media-amazon.com/images/M/MV5BMWZiNjE2OWItMTkwNy00ZWQzLWI0NT..."
+        Ratings= "7.7"
+    
+    
+    />).toJSON(); 
+    expect(tree).toMatchSnapshot(); 
+}); 
+
+*/
